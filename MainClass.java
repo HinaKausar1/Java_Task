c2223a4814a33883d642e3d55db59f01eb22f92e
HinaKausar1 <161328655+HinaKausar1@users.noreply.github.com>
2024-02-26 21:10:21 +0530

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,27 @@
+package Game;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class MainClass {
+
+	
+
+	public static void main(String[] args) {
+		
+		JFrame frame =new JFrame("Snake");
+		frame.setContentPane(new GamePanel());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.pack();
+		
+		
+		frame.setPreferredSize(new Dimension(GamePanel.HEIGHT, GamePanel.HEIGHT));
+	
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	
+	}
+
+}
